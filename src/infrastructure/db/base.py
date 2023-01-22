4b1cbecd50ca069a97d61a95510fccfdcf3b32f0
hhssome <artempavel-terehove@mail.ru>
2023-01-22 22:17:49 +0300

    added future mode

--- src/infrastructure/db/base.py
+++ src/infrastructure/db/base.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 def create_pool(database_url: str, echo_mode: bool) -> sessionmaker:
     logger.info('Create connections pool for DB')
 
-    engine = create_async_engine(url=database_url, echo=echo_mode)
+    engine = create_async_engine(url=database_url, echo=echo_mode, future=True)
     pool = sessionmaker(bind=engine, class_=AsyncSession,
                         expire_on_commit=False, autoflush=False)
     return pool
