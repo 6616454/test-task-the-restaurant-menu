8b75e5bce34ece674f48baf353095e7f15481b06
hhssome <artempavel-terehove@mail.ru>
2023-01-22 22:18:09 +0300

    new tests

---
+++ tests/test_api/menu/test_dish.py
@@ -0,0 +1,371 @@
+import uuid
+
+import pytest
+
+
+class TestDishHandlers:
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('menu_id, submenu_id, expected_result, status_code', [
+        (
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
+                '5f740121-65d6-490b-984c-1cb28a4b43fa',
+                [],
+                200
+        ),
+        (
+                str(uuid.uuid4()),
+                str(uuid.uuid4()),
+                {
+                    'detail': 'submenu not found'
+                },
+                404
+        )
+    ])
+    async def test_dishes_get(
+            self,
+            client,
+            menu_id,
+            submenu_id,
+            expected_result,
+            status_code,
+            menu_data,
+            create_menu_in_database,
+            submenu_data,
+            create_submenu_in_database
+    ):
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        response = client.get(
+            f'/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes'
+        )
+
+        assert response.json() == expected_result
+        assert response.status_code == status_code
+
+    @pytest.mark.asyncio
+    async def test_get_dishes_after_create(
+            self,
+            client,
+            submenu_data,
+            create_submenu_in_database,
+            menu_data,
+            create_menu_in_database,
+            dish_data,
+            create_dish_in_database
+    ):
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        await create_dish_in_database(**dish_data)
+        response = client.get(
+            f'api/v1/menus/{menu_data["menu_id"]}/submenus/{submenu_data["submenu_id"]}/dishes'
+        )
+
+        assert len(response.json()) == 1
+        assert response.status_code == 200
+
+    @pytest.mark.asyncio
+    async def test_create_submenu(
+            self,
+            client,
+            menu_data,
+            create_menu_in_database,
+            submenu_data,
+            create_submenu_in_database,
+            get_dish_from_database
+    ):
+        test_data = {
+            'title': 'some_title',
+            'description': 'some_description',
+            'price': '12.497'
+        }
+
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        response = client.post(
+            f'api/v1/menus/{menu_data["menu_id"]}/submenus/{submenu_data["submenu_id"]}/dishes',
+            json=test_data
+        )
+        data = response.json()
+
+        assert data['title'] == test_data['title']
+        assert data['description'] == test_data['description']
+
+        dish_from_db = await get_dish_from_database(data['id'])
+
+        assert data['title'] == dish_from_db.title
+        assert data['description'] == dish_from_db.description
+        assert data['price'] == dish_from_db.price
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('menu_id, submenu_id, test_data, expected_result, status_code', [
+        (
+                str(uuid.uuid4()),
+                str(uuid.uuid4()),
+                {
+                    'title': 'title',
+                    'description': 'description',
+                    'price': '12.49'
+                },
+                {
+                    'detail': 'submenu not found'
+                },
+                404
+        ),
+        (
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
+                '5f740121-65d6-490b-984c-1cb28a4b43fa',
+                {
+                    'price': '12.50'
+                },
+                {
+                    "detail": [
+                        {
+                            "loc": [
+                                "body",
+                                "title"
+                            ],
+                            "msg": "field required",
+                            "type": "value_error.missing"
+                        },
+                        {
+                            "loc": [
+                                "body",
+                                "description"
+                            ],
+                            "msg": "field required",
+                            "type": "value_error.missing"
+                        }
+                    ]
+                },
+                422
+        ),
+        (
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
+                '5f740121-65d6-490b-984c-1cb28a4b43fa',
+                {
+                    'title': 'title',
+                    'description': 'description'
+                },
+                {
+                    "detail": [
+                        {
+                            "loc": [
+                                "body",
+                                "price"
+                            ],
+                            "msg": "field required",
+                            "type": "value_error.missing"
+                        },
+                    ]
+                },
+                422
+        ),
+        (
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
+                '5f740121-65d6-490b-984c-1cb28a4b43fa',
+                {
+                    'title': 'title',
+                    'description': 'description',
+                    'price': 'string'
+                },
+                {
+                    'detail': 'the price of the dish must be a floating point number'
+                },
+                422
+        ),
+    ])
+    async def test_invalid_create_submenu(
+            self,
+            client,
+            menu_id,
+            submenu_id,
+            expected_result,
+            status_code,
+            test_data,
+            menu_data,
+            create_menu_in_database,
+            submenu_data,
+            create_submenu_in_database
+    ):
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        response = client.post(
+            f'api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes',
+            json=test_data
+        )
+
+        assert response.json() == expected_result
+        assert response.status_code == status_code
+
+    @pytest.mark.asyncio
+    async def test_get_dish(
+            self,
+            client,
+            menu_data,
+            submenu_data,
+            dish_data,
+            create_submenu_in_database,
+            create_menu_in_database,
+            create_dish_in_database,
+    ):
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        await create_dish_in_database(**dish_data)
+
+        response = client.get(
+            f'api/v1/menus/{menu_data["menu_id"]}/submenus/{submenu_data["submenu_id"]}/dishes/{dish_data["dish_id"]}'
+        )
+        data = response.json()
+
+        assert data['id'] == dish_data['dish_id']
+        assert data['title'] == dish_data['title']
+        assert data['description'] == dish_data['description']
+        assert data['price'] == dish_data['price']
+        assert response.status_code == 200
+
+    @pytest.mark.asyncio
+    async def test_get_dish_404(self, client):
+        response = client.get(
+            f'api/v1/menus/{str(uuid.uuid4())}/submenus/{str(uuid.uuid4())}/dishes/{str(uuid.uuid4())}'
+        )
+
+        assert response.status_code == 404
+        assert response.json() == {'detail': 'dish not found'}
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('dish_id, test_data, expected_result, status_code', [
+        (
+                '911577a1-fbf5-4931-b075-e7641c84121a',
+                {
+                    'title': 'new_title',
+                    'description': 'new_description',
+                    'price': '13.50'
+                },
+                {
+                    'id': '911577a1-fbf5-4931-b075-e7641c84121a',
+                    'title': 'new_title',
+                    'description': 'new_description',
+                    'price': '13.50'
+                },
+                200
+        ),
+        (
+                str(uuid.uuid4()),
+                {
+                    'title': 'new_title',
+                    'description': 'new_description'
+                },
+                {
+                    'detail': 'dish not found'
+                },
+                404
+        ),
+        (
+                '911577a1-fbf5-4931-b075-e7641c84121a',
+                {},
+                {
+                    'detail': 'dish_data request body empty'
+                },
+                400
+        ),
+        (
+                '911577a1-fbf5-4931-b075-e7641c84121a',
+                {
+                    'description': 'new_description2',
+                    'price': '13.50'
+                },
+                {
+                    'id': '911577a1-fbf5-4931-b075-e7641c84121a',
+                    'title': 'some_title',
+                    'description': 'new_description2',
+                    'price': '13.50'
+                },
+                200
+        ),
+        (
+                '911577a1-fbf5-4931-b075-e7641c84121a',
+                {
+                    'title': 'new_title2'
+                },
+                {
+                    'id': '911577a1-fbf5-4931-b075-e7641c84121a',
+                    'title': 'new_title2',
+                    'description': 'some_description',
+                    'price': '14.50'
+                },
+                200
+        )
+    ])
+    async def test_patch_dish(
+            self,
+            client,
+            dish_id,
+            test_data,
+            expected_result,
+            status_code,
+            menu_data,
+            submenu_data,
+            dish_data,
+            create_menu_in_database,
+            create_submenu_in_database,
+            create_dish_in_database,
+            get_dish_from_database
+
+    ):
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        await create_dish_in_database(**dish_data)
+        response = client.patch(
+            f'api/v1/menus/{menu_data["menu_id"]}/submenus/{submenu_data["submenu_id"]}/dishes/{dish_id}',
+            json=test_data
+        )
+        data = response.json()
+
+        assert data == expected_result
+        assert response.status_code == status_code
+
+        if data.get('id'):
+            dish_from_db = await get_dish_from_database(data['id'])
+
+            assert data['title'] == dish_from_db.title
+            assert data['description'] == dish_from_db.description
+            assert data['price'] == dish_from_db.price
+
+    @pytest.mark.asyncio
+    async def test_delete_dish(
+            self,
+            client,
+            menu_data,
+            submenu_data,
+            dish_data,
+            create_menu_in_database,
+            create_submenu_in_database,
+            create_dish_in_database,
+            get_dish_from_database
+    ):
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        await create_dish_in_database(**dish_data)
+        response = client.delete(
+            f'api/v1/menus/{menu_data["menu_id"]}/submenus/{submenu_data["submenu_id"]}/dishes/{dish_data["dish_id"]}'
+        )
+
+        assert response.json() == {
+            'status': True,
+            'message': 'The dish has been deleted'
+        }
+        assert response.status_code == 200
+
+        dish_from_db = await get_dish_from_database(dish_data['dish_id'])
+
+        assert dish_from_db is None
+
+    @pytest.mark.asyncio
+    async def test_delete_dish_404(self, client):
+        response = client.get(
+            f'api/v1/menus/{str(uuid.uuid4())}/submenus/{str(uuid.uuid4())}/dishes/{str(uuid.uuid4())}'
+        )
+
+        assert response.json() == {'detail': 'dish not found'}
+        assert response.status_code == 404
--- tests/test_api/menu/test_menu.py
+++ tests/test_api/menu/test_menu.py
@@ -3,21 +3,7 @@ import uuid
 import pytest
 
 
-class TestResultSaver:
-    menu_id: str = ''
-    submenu_id: str = ''
-
-    def set_menu(self, value: str):
-        self.menu_id = value
-
-    def set_submenu(self, value):
-        self.submenu_id = value
-
-
-test_result = TestResultSaver()
-
-
-class TestMenuScenario:
+class TestMenuHandlers:
     @pytest.mark.asyncio
     async def test_menus_get(self, client):
         response = client.get('/api/v1/menus')
@@ -26,7 +12,15 @@ class TestMenuScenario:
         assert response.status_code == 200
 
     @pytest.mark.asyncio
-    async def test_create_menu(self, client):
+    async def test_menus_get_after_create(self, client, create_menu_in_database, menu_data):
+        await create_menu_in_database(**menu_data)
+        response = client.get('api/v1/menus')
+
+        assert len(response.json()) == 1
+        assert response.status_code == 200
+
+    @pytest.mark.asyncio
+    async def test_create_menu(self, client, get_menu_from_database):
         test_data = {
             'title': 'test_title',
             'description': 'test_description'
@@ -34,13 +28,16 @@ class TestMenuScenario:
         response = client.post('api/v1/menus', json=test_data)
         data = response.json()
 
-        test_result.set_menu(str(data['id']))
-
         assert response.status_code == 201
         assert data['title'] == test_data['title']
         assert data['description'] == test_data['description']
         assert data['submenus_count'] == 0
         assert data['dishes_count'] == 0
+
+        menu_from_db = await get_menu_from_database(data['id'])
+
+        assert data['title'] == menu_from_db.title
+        assert data['description'] == menu_from_db.description
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize('data, expected_result, status_code', [
@@ -106,51 +103,36 @@ class TestMenuScenario:
         assert response.status_code == status_code
 
     @pytest.mark.asyncio
-    async def test_menus_get_after_create(self, client):
-        response = client.get('/api/v1/menus')
-        assert len(response.json()) == 1
+    async def test_get_menu(self, client, create_menu_in_database, menu_data):
+        await create_menu_in_database(**menu_data)
+        response = client.get(f'api/v1/menus/{menu_data["menu_id"]}')
+        data = response.json()
+
+        assert data['id'] == menu_data['menu_id']
+        assert data['title'] == menu_data['title']
+        assert data['description'] == menu_data['description']
+        assert data['submenus_count'] == 0
+        assert data['dishes_count'] == 0
+
         assert response.status_code == 200
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('data, expected_result, status_code', [
-        (
-                test_result.menu_id,
-                [
-                    {
-                        'id': '1',
-                        'title': 'test_title',
-                        'description': 'test_description',
-                        'submenus_count': 0,
-                        'dishes_count': 0
-                    }
-                ],
-                200
-        ),
-        (
-                str(uuid.uuid4()),
-                {
-                    'detail': 'menu not found'
-                },
-                404
-        )
-    ])
-    async def test_get_menu(self, client, data, expected_result, status_code):
-        response = client.get(f'api/v1/menus/{data}')
-        if isinstance(expected_result, list):
-            expected_result[0]['id'] = test_result.menu_id
-        assert response.json() == expected_result
-        assert response.status_code == status_code
+    async def test_get_menu_404(self, client):
+        response = client.get(f'api/v1/menus/{str(uuid.uuid4())}')
+
+        assert response.json() == {'detail': 'menu not found'}
+        assert response.status_code == 404
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('id_, test_data, expected_result, status_code', [
+    @pytest.mark.parametrize('menu_id, test_data, expected_result, status_code', [
         (
-                test_result.menu_id,
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
                 {
                     'title': 'new_title',
                     'description': 'new_description'
                 },
                 {
-                    'id': '1',
+                    'id': 'b61ec7b4-5b25-41de-9d41-f00331b04885',
                     'title': 'new_title',
                     'description': 'new_description',
                     'submenus_count': 0,
@@ -170,163 +152,125 @@ class TestMenuScenario:
                 404
         ),
         (
-                test_result.menu_id,
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
                 {},
                 {
                     'detail': 'menu_data request body empty'
                 },
-                200
+                400
         ),
         (
-                test_result.menu_id,
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
                 {
                     'description': 'new_description2'
                 },
                 {
-                    'id': '1',
-                    'title': 'new_title',
+                    'id': 'b61ec7b4-5b25-41de-9d41-f00331b04885',
+                    'title': 'some_title',
                     'description': 'new_description2',
                     'submenus_count': 0,
                     'dishes_count': 0
                 },
                 200
         ),
-    ])
-    async def test_patch_menu(self, client, id_, test_data, expected_result, status_code):
-        if id_:
-            response = client.patch(f'api/v1/menus/{id_}', json=test_data)
-        else:
-            response = client.patch(f'api/v1/menus/{test_result.menu_id}', json=test_data)
-
-        if expected_result.get('id'):
-            expected_result['id'] = test_result.menu_id
-
-        assert response.json() == expected_result
-        assert response.status_code == status_code
-
-    @pytest.mark.asyncio
-    @pytest.mark.parametrize('menu_id, expected_result, status_code', [
         (
-                str(uuid.uuid4()),
+                'b61ec7b4-5b25-41de-9d41-f00331b04885',
                 {
-                    'detail': 'menu not found'
+                    'title': 'new_title2'
                 },
-                404
-        ),
-        (
-                test_result.menu_id,
-                [],
+                {
+                    'id': 'b61ec7b4-5b25-41de-9d41-f00331b04885',
+                    'title': 'new_title2',
+                    'description': 'some_description',
+                    'submenus_count': 0,
+                    'dishes_count': 0
+                },
                 200
         )
     ])
-    async def test_get_submenus(self, client, menu_id, expected_result, status_code):
-        if menu_id:
-            response = client.get(f'api/v1/menus/{menu_id}/submenus')
-        else:
-            response = client.get(f'api/v1/menus/{test_result.menu_id}/submenus')
-
-        assert response.json() == expected_result
-        assert response.status_code == status_code
-
-    @pytest.mark.asyncio
-    async def test_create_submenu(self, client):
-        test_data = {
-            'title': 'test_title',
-            'description': 'description'
-        }
-
-        response = client.post(f'api/v1/menus/{test_result.menu_id}/submenus', json=test_data)
+    async def test_patch_menu(
+            self,
+            client,
+            menu_id,
+            test_data,
+            expected_result,
+            status_code,
+            menu_data,
+            create_menu_in_database,
+            get_menu_from_database
+    ):
+        await create_menu_in_database(**menu_data)
+        response = client.patch(f'api/v1/menus/{menu_id}', json=test_data)
         data = response.json()
 
-        test_result.set_submenu(str(data['id']))
+        assert data == expected_result
+        assert response.status_code == status_code
 
-        assert response.status_code == 201
-        assert data['title'] == test_data['title']
-        assert data['description'] == test_data['description']
-        assert data['dishes_count'] == 0
+        if data.get('id'):
+            menu_from_db = await get_menu_from_database(data['id'])
+
+            assert data['title'] == menu_from_db.title
+            assert data['description'] == menu_from_db.description
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize('menu_id, test_data, expected_result, status_code', [
-        (
-            str(uuid.uuid4()),
-            {
-                'title': 'title',
-                'description': 'description'
-            },
-            {
-                'detail': 'menu not found'
-            },
-            404
-        ),
-        (
-            test_result.menu_id,
-            {
-                'title': 'title'
-            },
-            {
-                "detail": [
-                    {
-                        "loc": [
-                            "body",
-                            "description"
-                        ],
-                        "msg": "field required",
-                        "type": "value_error.missing"
-                    }
-                ]
-            },
-            422
-        ),
-        (
-            test_result.menu_id,
-            {
-                'description': 'description'
-            },
-            {
-                "detail": [
-                    {
-                        "loc": [
-                            "body",
-                            "title"
-                        ],
-                        "msg": "field required",
-                        "type": "value_error.missing"
-                    }
-                ]
-            },
-            422
-        ),
-        (
-            test_result.menu_id,
-            {},
-            {
-                "detail": [
-                    {
-                        "loc": [
-                            "body",
-                            "title"
-                        ],
-                        "msg": "field required",
-                        "type": "value_error.missing"
-                    },
-                    {
-                        "loc": [
-                            "body",
-                            "description"
-                        ],
-                        "msg": "field required",
-                        "type": "value_error.missing"
-                    }
-                ]
-            },
-            422
-        )
-    ])
-    async def test_invalid_create_submenu(self, client, menu_id, test_data, expected_result, status_code):
-        if menu_id:
-            response = client.post(f'api/v1/menus/{menu_id}/submenus', json=test_data)
-        else:
-            response = client.post(f'api/v1/menus/{test_result.menu_id}/submenus', json=test_data)
+    async def test_delete_menu(self, client, menu_data, create_menu_in_database, get_menu_from_database):
+        await create_menu_in_database(**menu_data)
 
-        assert response.json() == expected_result
-        assert response.status_code == status_code
+        response = client.delete(f'api/v1/menus/{menu_data["menu_id"]}')
+
+        assert response.json() == {
+            'status': True,
+            'message': 'The menu has been deleted'
+        }
+        assert response.status_code == 200
+
+        menu_from_db = await get_menu_from_database(menu_data['menu_id'])
+
+        assert menu_from_db is None
+
+    @pytest.mark.asyncio
+    async def test_delete_menu_404(self, client):
+        response = client.get(f'api/v1/menus/{str(uuid.uuid4())}')
+
+        assert response.json() == {'detail': 'menu not found'}
+        assert response.status_code == 404
+
+    @pytest.mark.asyncio
+    async def test_counter_menu(
+            self,
+            client,
+            menu_data,
+            submenu_data,
+            dish_data,
+            create_menu_in_database,
+            create_submenu_in_database,
+            create_dish_in_database,
+            delete_submenu_from_database,
+            delete_dish_from_database
+    ):
+        await create_menu_in_database(**menu_data)
+        await create_submenu_in_database(**submenu_data)
+        await create_dish_in_database(**dish_data)
+
+        temp = dish_data['dish_id']
+        dish_data['dish_id'] = str(uuid.uuid4())
+        dish_data['title'] = 'new_title_dish'
+
+        await create_dish_in_database(**dish_data)
+
+        first_response = client.get(f'api/v1/menus/{menu_data["menu_id"]}')
+        first_data = first_response.json()
+
+        await delete_dish_from_database(dish_data['dish_id'])
+        await delete_dish_from_database(temp)
+        await delete_submenu_from_database(submenu_data['submenu_id'])
+
+        second_response = client.get(f'api/v1/menus/{menu_data["menu_id"]}')
+        second_data = second_response.json()
+
+        assert first_response.status_code == 200
+        assert second_response.status_code == 200
+        assert first_data['submenus_count'] == 1
+        assert first_data['dishes_count'] == 2
+        assert second_data['submenus_count'] == 0
+        assert second_data['dishes_count'] == 0
