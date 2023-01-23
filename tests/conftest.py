fac0d3ecdb9a96b525560c0cdc9aef54d7dfb709
hhssome <artempavel-terehove@mail.ru>
2023-01-23 18:49:37 +0300

    rename function / added func - close all session

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 import pytest_asyncio
 from fastapi import FastAPI
 from sqlalchemy import insert, text, delete, select
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, close_all_sessions
 from starlette.testclient import TestClient
 
 from src.api.di import setup_di
@@ -49,24 +49,25 @@ def client() -> Generator[TestClient, Any, None]:
 
 
 @pytest_asyncio.fixture(scope="session")
-async def async_session_test() -> sessionmaker:
+async def db_session_test() -> sessionmaker:
     yield create_pool(get_settings().database_test_url, echo_mode=False)
+    close_all_sessions()
 
 
 @pytest_asyncio.fixture(scope="function", autouse=True)
-async def clean_tables(async_session_test):
+async def clean_tables(db_session_test):
     tables = ('menu', 'submenu', 'dish')
-    async with async_session_test() as session:
+    async with db_session_test() as session:
         for table in tables:
             statement = text(f'''TRUNCATE TABLE {table} CASCADE;''')
             await session.execute(statement)
             await session.commit()
 
 
-@pytest_asyncio.fixture
-async def create_menu_in_database(async_session_test: sessionmaker):
+@pytest_asyncio.fixture(scope='function')
+async def create_menu_in_database(db_session_test: sessionmaker):
     async def create_menu_in_database(menu_id: str, title: str, description: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             await session.execute(insert(Menu).values(id=menu_id, title=title, description=description))
             await session.commit()
 
@@ -74,9 +75,9 @@ async def create_menu_in_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def create_submenu_in_database(async_session_test: sessionmaker):
+async def create_submenu_in_database(db_session_test: sessionmaker):
     async def create_submenu_in_database(submenu_id: str, title: str, description: str, menu_id: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             await session.execute(insert(SubMenu).values(
                 id=submenu_id,
                 title=title,
@@ -89,7 +90,7 @@ async def create_submenu_in_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def create_dish_in_database(async_session_test: sessionmaker):
+async def create_dish_in_database(db_session_test: sessionmaker):
     async def create_dish_in_database(
             dish_id: str,
             title: str,
@@ -97,7 +98,7 @@ async def create_dish_in_database(async_session_test: sessionmaker):
             price: str,
             submenu_id: str
     ):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             await session.execute(insert(Dish).values(
                 id=dish_id,
                 title=title,
@@ -111,9 +112,9 @@ async def create_dish_in_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def delete_menu_from_database(async_session_test: sessionmaker):
+async def delete_menu_from_database(db_session_test: sessionmaker):
     async def delete_menu_from_database(menu_id: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             query = delete(Menu).where(Menu.id == menu_id)
             await session.execute(query)
             await session.commit()
@@ -122,9 +123,9 @@ async def delete_menu_from_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def delete_submenu_from_database(async_session_test: sessionmaker):
+async def delete_submenu_from_database(db_session_test: sessionmaker):
     async def delete_submenu_from_database(submenu_id: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             query = delete(SubMenu).where(SubMenu.id == submenu_id)
             await session.execute(query)
             await session.commit()
@@ -133,9 +134,9 @@ async def delete_submenu_from_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def delete_dish_from_database(async_session_test: sessionmaker):
+async def delete_dish_from_database(db_session_test: sessionmaker):
     async def delete_dish_from_database(dish_id: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             query = delete(Dish).where(Dish.id == dish_id)
             await session.execute(query)
             await session.commit()
@@ -144,9 +145,9 @@ async def delete_dish_from_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def get_menu_from_database(async_session_test: sessionmaker):
+async def get_menu_from_database(db_session_test: sessionmaker):
     async def get_menu_from_database(menu_id: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             query = select(Menu).where(Menu.id == menu_id)
             return (await session.execute(query)).scalar_one_or_none()
 
@@ -154,9 +155,9 @@ async def get_menu_from_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def get_submenu_from_database(async_session_test: sessionmaker):
+async def get_submenu_from_database(db_session_test: sessionmaker):
     async def get_submenu_from_database(submenu_id: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             query = select(SubMenu).where(SubMenu.id == submenu_id)
             return (await session.execute(query)).scalar_one_or_none()
 
@@ -164,9 +165,9 @@ async def get_submenu_from_database(async_session_test: sessionmaker):
 
 
 @pytest_asyncio.fixture(scope='function')
-async def get_dish_from_database(async_session_test: sessionmaker):
+async def get_dish_from_database(db_session_test: sessionmaker):
     async def get_submenu_from_database(dish_id: str):
-        async with async_session_test() as session:
+        async with db_session_test() as session:
             query = select(Dish).where(Dish.id == dish_id)
             return (await session.execute(query)).scalar_one_or_none()
 
